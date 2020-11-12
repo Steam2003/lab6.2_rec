8760aa4d0e23ef77a819d9455f0134f568c0e18b
Ivan Patiuk <gendalf2003@gmail.com>
2020-11-12 13:39:34 +0200

    lab6.2_rec_fix

--- lab6.2_rec.cpp
+++ lab6.2_rec.cpp
@@ -3,29 +3,12 @@
 #include <iomanip>
 
 using namespace std;
-void min_max(int* mas, int min, int max, int i, int N)
-{
-	if (i <= N - 1)
-	{
-		if (mas[i] < min)
-			min = mas[i];
-		if (mas[i] > max)
-			max = mas[i];
-		return min_max(mas, min, max, ++i, N);
-	}
-	cout << endl;
-	cout << "Сума максимального та мінімального числа масиву : " << min + max << endl;
-}
-void rec(int* mas, const int Low, const int Max, int N, int i)
-{
-	if (i <= N - 1)
-	{
-		mas[i] = Low + rand() % (Max - Low + 1);
-		cout << setw(3) << mas[i] << " ";
-		return rec(mas, Low, Max, N, ++i);
-	}
-	else min_max(mas, mas[0], mas[0], 0, N);
-}
+
+void create(int* mas, const int Low, const int Max, int N, int i);
+void print(int* mas, int i, int N);
+int MinE(int* mas, int N, int i, int min);
+int MaxE(int* mas, int N, int i, int max);
+
 int main()
 {
 	int N, Low, Max;
@@ -38,5 +21,52 @@ int main()
 	cout << endl;
 	int* mas = new int[N];
 	cout << "Рекурсивний" << endl;
-	rec(mas, Low, Max, N, 0);
+	create(mas, Low, Max, N, 0);
+	print(mas, 0, N);
+	cout << endl;
+	int min = MinE(mas, N, 0, mas[0]);
+	int max = MaxE(mas, N, 0, mas[0]);
+	cout << "Мінімальне: " << min << endl;
+	cout << "Максимальне: " << max << endl;
+	cout << "Сума максимального та мінімального: " << min + max << endl;
+	delete[] mas;
+}
+void create(int* mas, const int Low, const int Max, int N, int i)
+{
+	if (i <= N - 1)
+	{
+		mas[i] = Low + rand() % (Max - Low + 1);
+		create(mas, Low, Max, N, ++i);
+	}
+}
+void print(int* mas, int i, int N)
+{
+	if (i <= N - 1)
+	{
+		cout << setw(3) << mas[i] << " ";
+		print(mas, ++i, N);
+	}
+
+}
+int MinE(int* mas, int N, int i, int min)
+{
+	if (i <= N - 1)
+	{
+		if (mas[i] < min)
+			min = mas[i];
+		MinE(mas, N, ++i, min);
+	}
+	else
+	return min;
+}
+int MaxE(int* mas, int N, int i, int max)
+{
+	if (i <= N - 1)
+	{
+		if (mas[i] > max)
+			max = mas[i];
+		MaxE(mas, N, ++i, max);
+	}
+	else
+	return max;
 }
